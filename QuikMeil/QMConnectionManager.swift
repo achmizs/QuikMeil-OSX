4889b923218df0d2aca0a4a9ebb7b86a2cdfc419
Said Achmiz <achmizs@gmail.com>
2016-03-14 16:32:46 -0400

    Fix includes

--- QuikMeil/QMConnectionManager.swift
+++ QuikMeil/QMConnectionManager.swift
@@ -14,7 +14,7 @@
 //	FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 //	License for more details.
 
-import Foundation
+import Cocoa
 
 // ************************
 // MARK: Useful functions
